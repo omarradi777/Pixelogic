59ea968f66d864e669c29a7fb2a1d8375b69c5d0
Omar Radi <omarradi@Ramis-MacBook-Air.local>
2020-10-18 00:20:13 +0200

    added a readme file

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# MyProjects
-Just a repository containing my projects
+# Automation [Pixelogic task]
+
+[Project description]
+​
+
+## Contents
+
+If you are looking for something specific, you can jump right into the relevant section from here.
+
+​1.[Getting Started](#getting-started)
+
+2.[Folder Structure](#folder-structure)
+
+3.[Videos](#style_guides)​
+
+4.[Reports](#style_guides)​
+
+​
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+​
+
+### About Cypress
+
+Cypress is a next generation front end testing tool built for the modern web. It can test anything that runs in a browser.
+For Cypress features, please check https://docs.cypress.io/guides/overview/why-cypress.html#Features
+Tests are written in cypress version 5.4.0
+​
+
+### Prerequisites
+
+1. [Node.js](https://nodejs.org/en/)
+
+### Instructions
+
+1.  Clone the repo
+
+```sh
+git clone [repo ssh]
+```
+
+2.  Install dependencies
+
+```sh
+npm install
+```
+
+3.  Open cypress
+
+```sh
+npm run cy:open
+```
+
+4.  Run test by clicking on it after cypress window opens.
+
+
+## Folder Structure
+
+| Folder      | Description                                                                                                                                                                                                                                                                 |
+| ----------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| Fixtures    | Used as external pieces of static data that can be used by your tests. Fixture files are located in cypress/fixtures by default, but can be configured to another directory.                                                                                                |
+| Integration | Used for creating test suits.Test files are located in cypress/integration by default, but can be configured to another directory.We are writing tests in js.Test file naming convention shall include the word test for easy documentation generation i.e feature1-test.js |
+| Plugin      | Used to load plugins By default.Cypress will automatically include the plugins file cypress/plugins/index.js before every single spec file it runs.                                                                                                                         |
+| Support     | Used to create various custom commands and contains class files for various methods used in the tests.
+                                                                                                                                               |
+
+## Videos
+
+Cypress provides a video recording to a successful test when ran by cy:run
+​
+
+## Reports
+
+These tests are intergrated with allure reports. To show the report you have to
+
+1.  Run Cypress headless
+
+```sh
+npm run cy:run
+```
+
+2.  Open allure reports
+
+```sh
+allure serve
+```
--- cypress/integration/register.js
+++ cypress/integration/register.js
@@ -18,46 +18,13 @@ describe("Registration tests", () => {
     var email = faker.internet.email();
     var password = generic.randomText(8)
 
-    before(function () {
+    beforeEach(function () {
         cy.fixture("register.json").as("register")
         cy.fixture("account.json").as("account")
         cy.fixture("login.json").as("login")
         control = new controls
         check = new checks
     })  
-
-    it.skip("Register as a user using API requests and verify registration by logging in to your account", function() {
-        /* Visiting register page and filling out register form to sign up */
-        cy.request({
-            method: 'POST',
-            url: 'https://www.phptravels.net/account/signup',
-            body: "firstname=Pamela&lastname=Stefanie&phone=(795)+789-3795&email=Dallasy82%40yahoo.com&password=brTgJiGI&confirmpassword=brTgJiGI"
-            // body: {
-            //     firstname: firstName,
-            //     lastname: lastName,
-            //     phone: mobileNumber,
-            //     email: email,
-            //     password : password,
-            //     confirmpassword: password,
-            // }
-        }).then((response)=>{
-            var x = response.body
-        })  
-        /* Check data are shown in the account page*/
-        cy.visit(this.register.url)
-        check.urlCheck(this.account.url)
-        check.visibleText(firstName)
-        check.visibleText(lastName)
-
-        /* Verify user can login with registerd credentials*/
-        control.clickOnGet(this.account.myAccountButton)
-        control.clickOnGet(this.account.logout)
-        check.urlCheck(this.login.url)
-        control.typeText(this.login.selectors.email,email)
-        control.typeText(this.login.selectors.password,password)
-        control.clickOnGet(this.login.loginButton)
-        check.urlCheck(this.account.url)
-    });
 
     it("Register as a user and verify registration by logging in to your account", function() {
         /* Visiting register page and filling out register form to sign up */
@@ -73,6 +40,31 @@ describe("Registration tests", () => {
         control.clickOnGet(this.register.button)
         
         /* Check data are shown in the account page*/
+        check.urlCheck(this.account.url)
+        check.visibleText(firstName)
+        check.visibleText(lastName)
+
+        /* Verify user can login with registerd credentials*/
+        control.clickOnGet(this.account.myAccountButton)
+        control.clickOnGet(this.account.logout)
+        check.urlCheck(this.login.url)
+        control.typeText(this.login.selectors.email,email)
+        control.typeText(this.login.selectors.password,password)
+        control.clickOnGet(this.login.loginButton)
+        check.urlCheck(this.account.url)
+    });
+
+    it("Register as a user using API requests and verify registration by logging in to your account", function() {
+        /* Visiting register page and filling out register form to sign up */
+        cy.request({
+            method: 'POST',
+            url: 'https://www.phptravels.net/account/signup',
+            body: "firstname="+firstName+"&lastname="+lastName+"&phone="+mobileNumber+"&email="+email+".com&password="+password+"&confirmpassword="+password
+        }).then((response)=>{
+            var x = response.body
+        })  
+        /* Check data are shown in the account page*/
+        cy.visit(this.register.url)
         check.urlCheck(this.account.url)
         check.visibleText(firstName)
         check.visibleText(lastName)
